7aea8b5f10557ad90103db18ad73f3003b45340e
Diogo Bento <diogo.bento@bringglobal.com>
2022-07-26 12:40:13 +0000

    feat: login page simple setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginPageApp';
+  email!: string;
+  password!: string;
+
+  loginUser() {
+    if(this.email === "admin@gmail.com" && this.password === "123456"){
+      console.log('Welcome!')
+    } else {
+      console.log('User Unauthorized')
+    }
+  }
 }
